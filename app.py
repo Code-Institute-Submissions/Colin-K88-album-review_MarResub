e8a30a60689c89a79fd1c13129fc477526fa0116
Colin-K88 <colinkee@hotmail.com>
2021-08-12 18:24:09 +0000

    add album template update

--- app.py
+++ app.py
@@ -103,6 +103,12 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_album")
+def add_album():
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("add_album.html", categories=categories)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
