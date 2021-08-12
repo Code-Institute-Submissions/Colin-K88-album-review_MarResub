58f5bec9be65405baed1b0858fd83506018a3e89
Colin-K88 <colinkee@hotmail.com>
2021-08-12 15:16:20 +0000

    app.py update

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import os
-from flask import Flask, render_template
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
@@ -14,7 +24,8 @@ def index():
 
 @app.route("/albums")
 def albums():
-    return render_template("albums.html")
+    albums = mongo.db.albums.find()
+    return render_template("albums.html", albums=albums)
 
 
 @app.route("/login")
@@ -22,7 +33,7 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     return render_template("register.html")
 
